5a4496b4a012ea93865ce1a02ef02717afb5d72f
Ben Przysucha <ben.mozilla@gmail.com>
2019-12-01 23:45:56 -0500

    first commit

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace NinetyNine.Menus
+{
+    public class MainMenu : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/PlayerNameInput.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+// ReSharper disable once CheckNamespace
+public class PlayerNameInput : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameInputField = null;
+    [SerializeField] private Button continueButton = null;
+
+    private const string PLAYER_PREFS_NAME_KEY = "PlayerName";
+
+    private void Start()
+    {
+        SetUpInputField();
+    }
+
+    private void SetUpInputField()
+    {
+        if (!PlayerPrefs.HasKey(PLAYER_PREFS_NAME_KEY)) return;
+
+        string defaultName = PlayerPrefs.GetString(PLAYER_PREFS_NAME_KEY);
+
+        nameInputField.text = defaultName;
+        SetPlayerName(defaultName);
+    }
+
+    public void SetPlayerName(string name)
+    {
+        continueButton.interactable = !string.IsNullOrEmpty(name);
+    }
+
+    public void SavePlayerName()
+    {
+        string playerName = nameInputField.text;
+        PhotonNetwork.NickName = playerName;
+        PlayerPrefs.SetString(PLAYER_PREFS_NAME_KEY, playerName);
+    }
+}
